94d88788298e7a5fcf696ea296a3efb0b9c95ac7
Marcelo Teixeira Cortes <marceloteixeiracortes@gmail.com>
2021-07-28 12:22:01 -0300

    added favicon

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://gateway.marvel.com', //process.env.REACT_APP_API,
+  baseURL: 'https://gateway.marvel.com', //process.env.REACT_APP_API,
 });
 
 export default api;
